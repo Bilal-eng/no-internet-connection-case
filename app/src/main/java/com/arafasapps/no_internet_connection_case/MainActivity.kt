293f85240f882a508248bec65271a8b5765431eb
Bilal <bilal.khalil@arafas.com>
2021-02-07 22:54:36 +0300

    initial commit.

---
+++ app/src/main/java/com/arafasapps/no_internet_connection_case/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.arafasapps.no_internet_connection_case
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
